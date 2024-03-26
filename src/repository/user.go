1651e45e7cabf489cb78bbe43be3c6c88452d9e0
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-03-26 18:36:56 +0900

    リポジトリパッケージにユーザー対話メソッドを追加
    CRUD (Create、Read、Update、Delete) 操作とユーザー認証用の追加メソッドを備えたユーザーリポジトリが導入されました。この追加により、データベース内のユーザーデータの管理が容易になり、やり取りが単純化されます。

---
+++ src/repository/user.go
@@ -0,0 +1,94 @@
+package repository
+
+import (
+	"errors"
+	"github.com/kouhei-github/golang-ddd-boboilerplate/models"
+
+	"gorm.io/gorm"
+)
+
+type User interface {
+	GetByID(id int) (*models.User, error)
+	GetByEmail(email string) (*models.User, error)
+	GetUserAuthByID(id int) (*models.UserAuth, error)
+	UpdateUserAuth(userAuth *models.UserAuth) error
+	Create(u *models.User) error
+	Update(u *models.User) error
+	Delete(id int) error
+}
+
+type user struct {
+	db gorm.DB
+}
+
+func NewUser(db gorm.DB) User {
+	return &user{
+		db: db,
+	}
+}
+
+func (u *user) GetByID(id int) (*models.User, error) {
+	user := models.User{}
+	err := u.db.Find(&user, id).Error
+	if err != nil {
+		return nil, err
+	}
+	if user.ID != id {
+		return nil, errors.New("not found")
+	}
+	return &user, nil
+}
+
+func (u *user) GetByEmail(email string) (*models.User, error) {
+	user := models.User{}
+	err := u.db.Where("email = ?", email).Find(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	if user.Email != email {
+		return nil, errors.New("not found")
+	}
+	return &user, nil
+}
+
+func (u *user) GetUserAuthByID(id int) (*models.UserAuth, error) {
+	userAuth := models.UserAuth{}
+	err := u.db.Where("user_id = ?", id).Find(&userAuth).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &userAuth, nil
+}
+
+func (u *user) UpdateUserAuth(userAuth *models.UserAuth) error {
+	err := u.db.Save(&userAuth).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *user) Create(user *models.User) error {
+	err := u.db.Create(&user).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *user) Update(user *models.User) error {
+	if user.ID == 0 {
+		return errors.New("id is not valid")
+	}
+
+	return u.db.Updates(&user).Error
+}
+
+func (u *user) Delete(id int) error {
+	if id == 0 {
+		return errors.New("id is not valid")
+	}
+
+	return u.db.Delete(&models.User{}, id).Error
+}

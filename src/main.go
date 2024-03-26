4d274ffcc1857e2ce6296edcd5b9fe3972f4607b
kouhei-github <kohei0801nagamatsu@gmail.com>
2024-03-26 16:42:29 +0900

    DatabaseProviderの追加とメインでの接続設定
    データベース接続を管理し簡素化するために、DatabaseProviderが確立されました。main関数では、アプリケーションの起動時に新しいDatabaseProviderを使用してデータベースへの接続が初期化されます。これにより、保守性とコードの可読性が向上します。

--- src/main.go
+++ src/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/kouhei-github/golang-ddd-boboilerplate/config"
+	"github.com/kouhei-github/golang-ddd-boboilerplate/provider"
 )
 
 func main() {
 	env := config.NewConfigENV()
 	env.EnvLoad()
+
+	database := provider.NewDatabaseProvider()
+	db, _, _ := database.Connect()
+	if db == nil {
+		panic("db is nil.")
+	}
+
 	app := fiber.New()
 	app.Get("/", helloHandler)
 	if err := app.Listen(":8888"); err != nil {
